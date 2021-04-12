9155464083fba6610e15151877f4dffd117a2b70
LeoLopez02 <leonellopez02@gmail.com>
2021-04-12 10:09:44 -0400

    added routes

--- server.py
+++ server.py
@@ -5,3 +5,18 @@ app = Flask(__name__)
 @app.route('/', methods="['GET']")
 def index():
   return app.send_static_file('index.html')
+
+
+@app.route('/home', methods="['GET']")
+def home():
+  return app.send_static_file('home.html')
+
+
+@app.route('/login', methods="['GET']")
+def login():
+  return app.send_static_file('login.html')
+
+
+@app.route('/signup', methods="['GET']")
+def signup():
+  return app.send_static_file('signup.html')
